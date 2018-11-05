155d5656ed8b62b72de8540b12ecfbbea0abffba
Brett Cimbalik <bcimbali@gmail.com>
2018-11-04 20:54:22 -0600

    Dynamically retrieve images from Wordpress Prior to this change, there was only a hard-coded image retrieved from Wordpress.
    This change retrieves multiple images from Wordpress and renders them on
    the Gatsby site.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,25 @@
-import Image from '../components/image'
+// import Image from '../components/image'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 import React from 'react'
 import { graphql } from 'gatsby'
 
 const IndexPage = ({ data }) => {
-  const testImage = data.allWordpressWpMedia.edges[0].node.source_url
+  // const testImage = data.allWordpressWpMedia.edges[0].node.source_url
   // const imageCaption = data.allWordpressWpMedia.edges[0].node.caption
   return (
     <Layout>
       {console.log('data: ', data)}
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Guino Foxes</p>
-      <img alt="Penguins walking" src={testImage} />
-      {data.allWordpressWpMedia.edges[0].node.caption}
-      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-        <Image />
-      </div>
+      <h1>Hi</h1>
+      <p>Gatsby and Wordpress Style Here</p>
+      {/* <img alt="Penguins walking" src={testImage} />
+      {data.allWordpressWpMedia.edges[0].node.caption} */}
+      {data.allWordpressWpMedia.edges.map(({ node }) => (
+        <div key={node.slug}>
+          <img key={node.id} alt={node.caption} src={node.source_url} />
+          <p>{node.caption}</p>
+        </div>
+      ))}
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
